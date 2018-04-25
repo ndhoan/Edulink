21f7bfceed65faf56d8e8a0e57ed7a784d2691a2
HOAN\Hoan <danghoanpro@outlook.com>
2018-04-25 12:57:59 +0700

    move form-notification

--- src/app/management/form-notification/form-notification.module.ts
+++ src/app/management/form-notification/form-notification.module.ts
@@ -1,12 +1,11 @@
-import { SidebarComponent } from './../sidebar.component';
-import { ManagementModule } from './../../management.module';
+import { ManagementModule } from './../management.module';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormNotificationComponent } from './form-notification.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
-import { TopbarComponent } from '../../topbar/topbar.component';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 
@@ -18,8 +17,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     ManagementModule,
-    TopbarComponent,
-    SidebarComponent,
     FormsModule,
     ReactiveFormsModule,
     BsDatepickerModule.forRoot(),
